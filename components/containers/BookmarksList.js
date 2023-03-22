e2d38a53519f245c4fe3b43a8951ecf8ab27de33
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-03-21 19:26:03 -0500

    Added patches

--- components/containers/BookmarksList.js
+++ components/containers/BookmarksList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FcFolder } from 'react-icons/fc';
 import Bookmark from './cards/Bookmark';
@@ -19,6 +19,7 @@ function BookmarksList({
   const buildTree = (node) => {
     const children = bookmarks.filter((child) => child.parentId === node.id);
     if (children.length === 0) {
+      console.warn(node);
       return node;
     }
     return {
@@ -28,25 +29,34 @@ function BookmarksList({
   };
 
   const tree = buildTree(rootNode);
-
+  useEffect(() => {
+  }, [bookmarks]);
   return (
     <div className="bookmark-list_container">
       <span>
         <FcFolder />
         {shortenedString(rootNode.title)}
       </span>
-      {tree.children.map((child) => (
-        <span key={child.id}>
-          <Bookmark
-            key={child.id}
-            node={child}
-            bookmarks={bookmarks}
-            resources={resources}
-            toggledFilter={toggledFilter}
-            handleShowForm={handleShowForm}
-          />
-        </span>
-      ))}
+      {tree.children
+        ? (
+          <>
+            {tree.children.map((child) => (
+              <span key={child.id}>
+                <Bookmark
+                  key={child.id}
+                  node={child}
+                  bookmarks={bookmarks}
+                  resources={resources}
+                  toggledFilter={toggledFilter}
+                  handleShowForm={handleShowForm}
+                />
+              </span>
+            ))}
+          </>
+        )
+        : (
+          ''
+        )}
     </div>
   );
 }
