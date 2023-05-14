e629e490e78e8fbf95793ac724215d7c8221fc66
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2023-05-13 20:16:14 -0500

    Added components to index, added state handling and condional rendering

--- components/containers/LearnedTechView.js
+++ components/containers/LearnedTechView.js
@@ -1,16 +1,37 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import LearnedTechCard from './cards/LearnedTechCard';
+import { getAllGoals } from '../../utils/data/goals';
+import { getAllTopics } from '../../utils/data/topics';
+import { useAuth } from '../../utils/context/authContext';
+import RecentsList from './RecentsList';
+import Loading from '../Loading';
 
 export default function LearnedTechView({ tech }) {
   const router = useRouter();
+  const { user } = useAuth();
+  const [goals, setGoals] = useState([]);
+  const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const handleClick = (obj) => {
     router.push({
       pathname: `/lTech/${obj.id}`,
       query: { tech: obj.tech.id },
     });
   };
+  const getDataAndSetState = () => {
+    Promise.all([getAllGoals(user), getAllTopics(user)])
+      .then(([a, b]) => {
+        setGoals(a);
+        setTopics(b);
+        setIsLoading(false);
+      });
+  };
+  useEffect(() => {
+    getDataAndSetState();
+  }, [user]);
   return (
     <>
       <div className="tech-view_container">
@@ -35,6 +56,13 @@ export default function LearnedTechView({ tech }) {
             Expand
           </span>
         </div>
+        <div className="flex-row margin-l-md gap-col">
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <RecentsList list={[...goals]} />
+          )}
+        </div>
         <div className="flex-row space-between_shift-down">
           <span className="sub-heading padding">
             Topics
@@ -42,6 +70,13 @@ export default function LearnedTechView({ tech }) {
           <span className="sub-heading-sm padding">
             Expand
           </span>
+        </div>
+        <div className="flex-row margin-l-md gap-col">
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <RecentsList list={[...topics]} />
+          )}
         </div>
       </div>
     </>
